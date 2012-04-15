9f5f7e98203edaa5e560deb8c527af08b4020a79
Saeed Abdullah <sma249@cornell.edu>
2012-04-14 21:58:17 -0400

    Adds run script for creating count matrix.

--- tagger/datautil.py
+++ tagger/datautil.py
@@ -71,3 +71,50 @@ def get_tag_n_gram(n, filename):
 
     return count_matrix
 
+
+def create_count_matrix(filename, output_dir):
+    """Creates count matrix from the training file.
+
+    It reads the training file and saves the count matrices for
+    unigram, bigram, trigram and tag-word in the count_matrix/
+    sub-directory created in the given output_dir.
+
+    param
+    ----
+    filename: Path of training file.
+    output_dir: Output directory.
+    """
+
+    import os
+    import json
+
+    word_tag_output = "tag_word_count.json"
+    bigram_matrix_name = "bigram_count.json"
+    unigram_matrix_name = "unigram_count.json"
+    trigram_matrix_name = "trigram_count.json"
+
+    sub_dir = os.path.join(output_dir, "count_matrix/")
+    if not os.path.exists(sub_dir):
+        os.mkdir(sub_dir)
+
+    word_tag_matrix = get_tag_word_matrix(filename)
+    with open(sub_dir + word_tag_output, "w") as f:
+        json.dump(word_tag_matrix, f)
+
+    unigram_matrix = get_tag_n_gram(n=1, filename=filename)
+    with open(sub_dir + unigram_matrix_name, "w") as f:
+        json.dump(unigram_matrix, f)
+
+    bigram_matrix = get_tag_n_gram(n=2, filename=filename)
+    with open(sub_dir + bigram_matrix_name, "w") as f:
+        json.dump(bigram_matrix, f)
+
+    trigram_matrix = get_tag_n_gram(n=3, filename=filename)
+    with open(sub_dir + trigram_matrix_name, "w") as f:
+        json.dump(trigram_matrix, f)
+
+
+if __name__=="__main__":
+    import sys
+
+    create_count_matrix(sys.argv[1], sys.argv[2])
